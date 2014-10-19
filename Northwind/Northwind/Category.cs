209b9d973de85f5627f5f40414cd838748f7aea8
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-10-19 23:36:43 +0200

    a

---
+++ Northwind/Northwind/Category.cs
@@ -0,0 +1,16 @@
+﻿using LINQtoCSV;
+
+namespace Northwind
+{
+    public class Category
+    {
+        [CsvColumn(Name = "CategoryID")]
+        public long Id { get; set; }
+
+        [CsvColumn(Name = "CategoryName")]
+        public string Name { get; set; }
+
+        [CsvColumn(Name = "Description")]
+        public string Description { get; set; }
+    }
+}
---
+++ Northwind/Northwind/Order.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using LINQtoCSV;
+
+namespace Northwind
+{
+    public class Order
+    {
+        [CsvColumn(Name = "OrderID")]
+        public long OrderId { get; set; }
+
+        [CsvColumn(Name = "CustomerID")]
+        public string CustomerId { get; set; }
+
+        [CsvColumn(Name = "EmployeeID")]
+        public long EmployeeId { get; set; }
+
+        [CsvColumn(Name = "OrderDate")]
+        public DateTime OrderDate { get; set; }
+
+        [CsvColumn(Name = "RequiredDate")]
+        public DateTime RequiredDate { get; set; }
+
+        [CsvColumn(Name = "ShippedDate")]
+        public DateTime ShippedDate { get; set; }
+
+        [CsvColumn(Name = "ShipVia")]
+        public int ShipVia { get; set; }
+
+        [CsvColumn(Name = "Freight")]
+        public double Freight { get; set; }
+
+        [CsvColumn(Name = "ShipName")]
+        public string ShipName { get; set; }
+
+        [CsvColumn(Name = "ShipAddress")]
+        public string ShipAddress { get; set; }
+
+        [CsvColumn(Name = "ShipCity")]
+        public string ShipCity { get; set; }
+
+        [CsvColumn(Name = "ShipRegion")]
+        public string ShipRegion { get; set; }
+
+        [CsvColumn(Name = "ShipPostalCode")]
+        public string ShipPostalCode { get; set; }
+
+        [CsvColumn(Name = "ShipCountry")]
+        public string ShipCountry { get; set; }
+    }
+}
