2999caf409c0872ba32a2b2896ebae957339238a
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-10-20 17:26:01 +0200

    Works

--- Northwind/Northwind/Program.cs
+++ Northwind/Northwind/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Northwind
 {
@@ -10,10 +11,33 @@ namespace Northwind
             var r = new Repository();
             r.LoadFiles();
 
-            NorthWind nw = new NorthWind(r);
+            var nw = new NorthWind(r);
 
             // Subscriber..
-            
+            nw.NewOrder += NewOrder_Load;
+
+            nw.AddOrder("Doegn Netto", "Rued Langgaards Vej 23a", "Copenhagen", "South", "2300", "Denmark");
+
+            Console.WriteLine();
+            Console.WriteLine("First five products:");
+            List<Product> firstFiveProducts = nw.Products.Take(5).ToList();
+            firstFiveProducts.ForEach(product => Console.WriteLine(product.Name));
+
+            Console.WriteLine();
+            var shippingCountryCount =
+                nw.Orders.GroupBy(order => order.ShipCountry)
+                    .Select(shipCountry => new
+                    {
+                        ShipCountry = shipCountry.Key,
+                        Count = shipCountry.Count()
+                    }).OrderByDescending(x => x.Count).ToList();
+            shippingCountryCount.ForEach(
+                shippingCountry => Console.WriteLine(shippingCountry.ShipCountry + " - " + shippingCountry.Count));
+        }
+
+        private static void NewOrder_Load(object sender, NorthWind.NewOrderEventArgs e)
+        {
+            Console.WriteLine("New Order Added:\nID: " + e.orderId + "\nDate: " + e.orderDate);
         }
     }
 }
