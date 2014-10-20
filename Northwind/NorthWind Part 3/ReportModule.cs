49f72bacb2d891200d273a0ad19628311d601f6a
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-10-21 00:20:15 +0200

    shazaaam

--- Northwind/NorthWind Part 3/ReportModule.cs
+++ Northwind/NorthWind Part 3/ReportModule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,14 +30,16 @@ namespace NorthWind_Part_3
         public Report<IList<OrdersByTotalPriceDto>, ReportError> TopOrdersByTotalPrice(int count)
         {
 
-            var topOrdersByTotalPrice = _context.Orders.OrderByDescending(order => order.Order_Details.Sum(od => od.UnitPrice * od.Quantity));
+            var topOrdersByTotalPrice =
+                _context.Orders.OrderByDescending(order => order.Order_Details.Sum(od => od.UnitPrice*od.Quantity));
             var topCountOrdersByTotalPrice = topOrdersByTotalPrice.Take(count).Select(order => new OrdersByTotalPriceDto
             {
                 OrderId = order.OrderID,
                 OrderDate = order.OrderDate,
                 CustomerContactName = order.Customers.ContactName,
-                TotalPriceWithDiscount = order.Order_Details.Sum(od => (1 - (decimal) od.Discount) * od.UnitPrice * od.Quantity),
-                TotalPrice = order.Order_Details.Sum(od => od.UnitPrice * od.Quantity)
+                TotalPriceWithDiscount =
+                    order.Order_Details.Sum(od => (1 - (decimal) od.Discount)*od.UnitPrice*od.Quantity),
+                TotalPrice = order.Order_Details.Sum(od => od.UnitPrice*od.Quantity)
             }).ToList();
 
             return new Report<IList<OrdersByTotalPriceDto>, ReportError>()
@@ -80,6 +83,58 @@ namespace NorthWind_Part_3
 
             return new Report<IList<ProductsBySaleDto>, ReportError>() {Data = topCountProductsBySale, Error = null};
         }
+
+        public Report<EmployeeSaleDto, ReportError> EmployeeSale(int id)
+        {
+
+            try
+            {
+                var employee = (from e in _context.Employees
+                    where e.EmployeeID == id
+                    select e).First();
+
+                string employeeName = employee.FirstName + " " + employee.LastName;
+                int? reportsToId = employee.ReportsTo;
+
+
+                var ordersDto = (from o in employee.Orders
+                    select new OrdersDto
+                    {
+                        OrderID = o.OrderID,
+                        OrderDate = o.OrderDate,
+                        TotalPrice = o.Order_Details.Sum(od => od.UnitPrice*od.Quantity),
+                        Products = (from od in o.Order_Details
+                            select new ProductsDto
+                            {
+                                ProductName = od.Product.ProductName,
+                                UnitPrice = od.UnitPrice,
+                                Quantity = od.Quantity
+                            }).ToList()
+                    }).ToList();
+
+                return new Report<EmployeeSaleDto, ReportError>
+                {
+                    Data = new EmployeeSaleDto
+                    {
+                        EmployeeName = employeeName,
+                        ReportsToId = reportsToId,
+                        Orders = ordersDto
+                    },
+                    Error = null
+                };
+
+                return null;
+            }
+            catch (ArgumentNullException e)
+            {
+                return new Report<EmployeeSaleDto, ReportError>
+                {
+                    Data = null,
+                    Error = new ReportError() { ErrorMessage = "No Employee found with ID: " + id}
+                };
+
+            }
+        }
     }
 
     public class ReportError
@@ -121,10 +176,29 @@ namespace NorthWind_Part_3
         public int Month { get; set; }
         public int Year { get; set; }
     }
-    }
-    }
+
+
     public class EmployeeSaleDto
     {
-        
+        public string EmployeeName { get; set; }
+        public int? ReportsToId { get; set; }
+        public List<OrdersDto> Orders { get; set; }
+
+    }
+
+    public class OrdersDto
+    {
+        public int OrderID { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<ProductsDto> Products { get; set; }
+    }
+
+    public class ProductsDto
+    {
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+
     }
 }
