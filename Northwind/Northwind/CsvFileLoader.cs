574c13a05bff1b76e5400a269d8a7e1ef9f54b60
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-10-31 15:15:24 +0100

    test

---
+++ Northwind/Northwind/CsvFileLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindNS
+{
+    class CsvFileLoader : ICsvFileLoader
+    {
+        public string[] categoryLines { get; private set; }
+        public string[] productLines { get; private set; }
+        public string[] orderLines { get; private set; }
+        public string[] orderDetailLines { get; private set; }
+
+        public CsvFileLoader()
+        {
+            // Skip 1 because of the headers.
+            categoryLines = File.ReadAllLines(@"../../Resources/categories.csv").Skip(1).ToArray();
+            productLines = File.ReadAllLines(@"../../Resources/products.csv").Skip(1).ToArray();
+            orderLines = File.ReadAllLines(@"../../Resources/orders.csv").Skip(1).ToArray();
+            orderDetailLines = File.ReadAllLines(@"../../Resources/order_details.csv").Skip(1).ToArray();
+        }
+    }
+}
---
+++ Northwind/Northwind/ICsvFileLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindNS
+{
+    public interface ICsvFileLoader
+    {
+        string[] categoryLines { get; }
+        string[] productLines { get; }
+        string[] orderLines { get; }
+        string[] orderDetailLines { get; }
+    }
+}
